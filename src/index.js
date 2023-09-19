b2df50845c18a9bb19a6d98633cdf100dd2e1d57
anthonyruvoen <anthonyruvoen@gmail.com>
2023-09-19 16:53:45 +0200

    sum.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import _ from 'lodash';
+import _ from 'lodash'
+import '../src/scss/styles.scss'
 
-function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+function component () {
+  const element = document.createElement('div')
+  element.classList.add('container')
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+
+  return element
+}
+
+document.body.appendChild(component())
